f1b5ab9c005e86a6b77c5606bc94fa6f5dfcb7b5
ne91rywu <marie.t.schmid@fau.de>
2024-08-17 17:55:10 +0200

    Changed .wkt file selection to path input

---
+++ CuraEngineTiledInfill/constants.py
@@ -0,0 +1,5 @@
+
+cura_plugin_name = "CuraEngineLayeredInfill"
+version = "1.0.0"
+curaengine_plugin_name = "curaengine_plugin_infill_generate"
+settings_prefix = "_plugin__curaenginelayeredinfill__0_2_0_"
